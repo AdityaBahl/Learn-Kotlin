821f2289c34711b85a953cfb0e1b54d5824e6319
AdityaBahl <adityabahl12345@gmail.com>
2023-01-15 21:47:45 +0530

    For Each Loop

---
+++ forEachLoop.kt
@@ -0,0 +1,7 @@
+package com.example.helloworld
+fun main(args:Array<String>){
+	val listt=listOf<String>("get","on","the","dance","floor")
+	listt.forEach{
+	println("$it -->")//it is the default thing used here
+	}
+}
